f739843474764e38760c13ab2deec798bf8e3360
Itsfazo28 <fadhilimbaga99@gmail.com>
2025-01-06 01:14:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "FAZ×GAMING"
 
-global.banknumber = "8133729715"
+global.banknumber = "255622048500 "
 
-global.bankowner = "FADARE"
+global.bankowner = "FAZO"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
